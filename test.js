6baf102841ee65e4f01ebd2f596c753dff74d008
fahim-qubryx <fahim.asharaf@qubryx.com>
2024-05-18 15:41:21 +0530

    feat(app): Add frequency

--- test.js
+++ test.js
@@ -4,9 +4,11 @@ try {
   // var interval = parser.parseExpression('*/2 * * * *');
   // var interval = parser.parseExpression('0 0 1L * ?');
   // var interval = parser.parseExpression('0 0 ? * 3,4');
-  var interval = parser.parseExpression('0 0 ? * 4,5', {frequency: 1, frequencyType: 'weekly'} );
+  // var interval = parser.parseExpression('20 7 10 * ?', {includeCurrentDate: true, currentDate: new Date('Mon, 10 Apr 2023 12:38:53 UTC'), frequency: 1, frequencyType: 'monthly'} );
+  // var interval = parser.parseExpression('0 0 10 * ?', { frequency: 1, frequencyType: 'monthly'} );
   // var interval = parser.parseExpression('0 0 31 * ?', {frequency: 1, frequencyType: 'monthly', includeCurrentDate: false} );
-  // var interval = parser.parseExpression('0 0 20 * ?', {frequency: 10, frequencyType: 'monthly'} );
+  // var interval = parser.parseExpression('0 9 ? * 1,3,4', {frequency: 1, frequencyType: 'weekly', currentDate: new Date('2023-04-04T00:00:00.000Z')} );
+  var interval = parser.parseExpression('0 0 ? * 1,3,4', {frequency: 1, frequencyType: 'weekly',tz: 'Asia/Calcutta', currentDate: new Date('2023-03-01T00:00:00.000Z')} );
 
   console.log('Date: ', interval.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
   console.log('Date: ', interval.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
