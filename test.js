4734719a5b6d751380da72bb1fc289b9b3211816
Fahim <fahim@Macbook-Pro-M2-Fahim.local>
2024-05-28 11:08:13 +0530

    Support multiple cron expresions and repeatFor option

--- test.js
+++ test.js
@@ -8,14 +8,57 @@ try {
   // var interval = parser.parseExpression('0 0 10 * ?', { frequency: 1, frequencyType: 'monthly'} );
   // var interval = parser.parseExpression('0 0 31 * ?', {frequency: 1, frequencyType: 'monthly', includeCurrentDate: false} );
   // var interval = parser.parseExpression('0 9 ? * 1,3,4', {frequency: 1, frequencyType: 'weekly', currentDate: new Date('2023-04-04T00:00:00.000Z')} );
-  var interval = parser.parseExpression('0 0 ? * 1,3,4', {frequency: 1, frequencyType: 'weekly',tz: 'Asia/Calcutta', currentDate: new Date('2023-03-01T00:00:00.000Z')} );
+  // var interval = parser.parseExpression('0 0 ? * 5,3,4', {repeatFor: 3});
+  // var interval1 = parser.parseExpressions(['0 0 ? * 5', '0 0 ? * 3','0 0 ? * 4']);
+  // var interval2 = parser.parseCronExpressions(['0 0 ? * 5', '0 0 ? * 3','0 0 ? * 4']);
+  //var interval3 = parser.parseExpression('0 0 20 * ?');
 
-  console.log('Date: ', interval.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
-  console.log('Date: ', interval.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
-  console.log('Date: ', interval.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
- 
-  // console.log('Date: ', interval.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+  // TEST CASES ::
+  // Case 1: Month Option: Standard and Date and Repeat for. Or Day in Custom option and Order not equal to Last
+  // var interval2 = parser.parseCronExpressions(['0 0 20 * ?'], {frequency: 3, frequencyType:"monthly"});
+  // var interval2 = parser.parseCronExpressions(['0 0 20 * ?'], {frequency: 3, frequencyType:"monthly", currentDate: new Date('2024-06-22T18:30:00.000Z')});
+  // var interval2 = parser.parseCronExpressions(['0 0 20 * ?'], {repeatFor:5, repeatType: 'days', frequency: 3, frequencyType:"monthly", currentDate: new Date('2024-06-22T18:30:00.000Z')});
+  // CASE 2:
+  // var interval2 = parser.parseCronExpressions(['0 0 1W * ?'], {repeatFor:5, repeatType: 'days', frequency: 2, frequencyType:"monthly", currentDate: new Date('2024-06-22T18:30:00.000Z')});
+  // CASE 3:
+  // var interval2 = parser.parseCronExpressions(['0 0 ? * 0#2'], {isFullWeek: true, repeatFor:2, repeatType: 'weeks', frequency: 2, frequencyType:"monthly", currentDate: new Date('2024-06-22T18:30:00.000Z')});
+  // var interval2 = parser.parseCronExpressions(['0 0 ? * 5L'], {isFullWeek: true, repeatFor:2, repeatType: 'weeks', frequency: 2, frequencyType:"monthly", currentDate: new Date('2024-06-22T18:30:00.000Z')});
+  // CASE 4:
+  // var interval2 = parser.parseCronExpressions(["5 10 ? * 1L"], {repeatFor:2, repeatType: 'weeks', frequency: 1, frequencyType:"monthly", currentDate: new Date('2024-06-22T18:30:00.000Z')});
+
+
+  // var interval2 = parser.parseExpression("5 10 25 * ?", {frequency: 1, frequencyType:"monthly", endDate: new Date('2024-07-27T18:30:00.000Z')});
+  // var interval2 = parser.parseCronExpressions(["5 10 25 * ?"], {repeatFor:3, repeatType: 'days', frequency: 1, frequencyType:"monthly",currentDate: new Date('2024-06-20T18:30:00.000Z'), endDate: new Date('2024-07-26T18:30:00.000Z')});
+  // var interval2 = parser.parseCronExpressions([ '0 0 ? * 1#1', '0 0 ? * 3#1', '0 0 ? * 5#1' ], {repeatFor:2, repeatType: 'days', frequency: 1, frequencyType:"monthly",currentDate: new Date('2024-06-05T18:30:00.000Z'), endDate: new Date('2024-06-29T18:30:00.000Z')});
+  // var interval2 = parser.parseExpression("5 10 L * ?", {frequency: 1, frequencyType:"monthly", endDate: new Date('2024-07-28T18:30:00.000Z')});
+  // var interval2 = parser.parseCronExpressions(["5 10 L * ?"], {repeatFor:5, repeatType: 'days', frequency: 1, frequencyType:"monthly", sDate: new Date('2024-05-20T18:30:00.000Z'), endDate: new Date('2024-06-10T18:30:00.000Z')});
+  // var interval2 = parser.parseCronExpressions(["5 10 L * ?"], {repeatFor:5, repeatType: 'days', frequency: 1, frequencyType:"monthly", currentDate: new Date('2024-05-20T18:30:00.000Z'), endDate: new Date('2024-06-28T18:30:00.000Z')});
+  // var interval2 = parser.parseExpression("5 10 L * ?", {repeatFor:5, repeatType: 'days', frequency: 1, frequencyType:"monthly", currentDate: new Date('2024-05-20T18:30:00.000Z'), endDate: new Date('2024-05-10T18:30:00.000Z')});
+  // var interval2 = parser.parseCronExpressions(["5 10 L * ?"], {repeatFor:5, repeatType: 'days', frequency: 1, frequencyType:"monthly"});
+
+  // console.log('Date 3: ', interval3.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+  // console.log('Date 3: ', interval3.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+  // console.log('Date: ', interval1); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+  // console.log('Date1: ', interval.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+  // console.log('Date1: ', interval.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+  // console.log('Date1: ', interval.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+  console.log('Date1: ', interval.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+  console.log('Date2: ', interval2.hasNext()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+  console.log('Date2: ', interval2.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+  console.log('Date2: ', interval2.hasNext()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+  console.log('Date2: ', interval2.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+  console.log('Date2: ', interval2.hasNext()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+  console.log('Date2: ', interval2.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+
 
 } catch (err) {
   console.log('Error: ' + err.message);
 }
+
+/*
+  options = {
+    repeatFor: number
+    repeatType: weeks, days, workingDays
+    isFullWeek: true/false
+  }
+*/
